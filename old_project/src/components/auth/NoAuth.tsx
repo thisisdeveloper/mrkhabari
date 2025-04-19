cbfad8112ac0999a5f36ae1517d6540ec00ae629
Sachin Rajput <sachinrajput@Sachins-MacBook-Air.local>
2025-04-20 01:19:31 +0530

    old folder deleted

--- old_project/src/components/auth/NoAuth.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function NoAuth() {
-  return (
-    <div className="text-sm text-gray-500">
-      <p>No authentication will be added to the request</p>
-    </div>
-  );
-}
